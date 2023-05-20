cb4497ff845ed02104177bbd6ff26fa2f65beb8e
Ariva2 <ariva080503@gmail.com>
2023-05-20 21:02:02 +0700

    Initial commit

---
+++ src/main/java/ariva/perpustakaanapp/PerpustakaanApp.java
@@ -0,0 +1,7 @@
+package ariva.perpustakaanapp;
+public class PerpustakaanApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/db/Koneksi.java
@@ -0,0 +1,35 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Koneksi {
+    
+    private final String URL = "jdbc:mysql://localhost:3306/db_perpus";
+    private final String USER = "root";
+    private final String PASS = "";
+    
+    public Connection getConnection() {
+        Connection con;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(URL,USER,PASS);
+            System.out.println("Koneksi Berhasil");
+            return con;
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Koneksi.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Koneksi Gagal.");
+            return null;
+        }
+          
+    }
+        
+        public static void main(String[] args) {
+            Koneksi koneksi = new Koneksi ();
+            koneksi.getConnection();
+        }        
+    }
